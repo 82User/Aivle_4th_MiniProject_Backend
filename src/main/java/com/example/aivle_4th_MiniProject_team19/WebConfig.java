e39a9f3662a12c037a22203bdf4458426869c17b
82User <zxcvc9999@gmail.com>
2025-12-19 16:44:08 +0900

    cors 수정

--- src/main/java/com/example/aivle_4th_MiniProject_team19/WebConfig.java
+++ src/main/java/com/example/aivle_4th_MiniProject_team19/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE") // HTTP 메서드 허용
                 .allowedHeaders("*")                                    // 모든 헤더 허용
-                .allowCredentials(true);                                // 쿠키 인증 요청 허용
+                .allowCredentials(false);                                // 쿠키 인증 요청 허용
     }
 }
