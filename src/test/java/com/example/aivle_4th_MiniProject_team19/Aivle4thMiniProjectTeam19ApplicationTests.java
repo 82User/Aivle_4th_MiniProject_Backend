e3340a095145bed763593c5cf7880cb8988dd700
82User <zxcvc9999@gmail.com>
2025-12-23 10:35:19 +0900

    yml 수ì정

--- src/test/java/com/example/aivle_4th_MiniProject_team19/Aivle4thMiniProjectTeam19ApplicationTests.java
+++ src/test/java/com/example/aivle_4th_MiniProject_team19/Aivle4thMiniProjectTeam19ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.aivle_4th_MiniProject_team19;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Aivle4thMiniProjectTeam19ApplicationTests {
 
 	@Test
